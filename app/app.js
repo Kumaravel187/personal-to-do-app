a9cae93a90ca5d0e1d543f8e16c952786ed06b65
Kumaravel187 <46121794+Kumaravel187@users.noreply.github.com>
2023-08-14 14:19:19 +0800

    Added in functionality
    - Added vanilla js to allow users to add and delete tasks
    - Added stylings for ::before selectors
    - Used local storage to store list items

--- app/app.js
+++ app/app.js
@@ -1,1 +1,51 @@
-console.log("Hi");
+const inputEl = document.getElementById("input-box");
+const listEl = document.getElementById("list-container");
+
+const taskBtn = document.getElementById("btn");
+
+taskBtn.addEventListener("click", addTask);
+
+function addTask() {
+  // Check if user writes in an input
+  if (inputEl.value === "") {
+    alert("You must write something");
+  } else {
+    // create a list element and upload the input value as a child list for the list container
+    let li = document.createElement("li");
+    li.innerHTML = inputEl.value;
+    listEl.appendChild(li);
+
+    let span = document.createElement("span");
+    // code for x icon
+    span.innerHTML = "\u00d7";
+    // NOTE:APPEND TO LI NOT UL
+    li.appendChild(span);
+  }
+  // clear input value
+  inputEl.value = "";
+  saveData();
+}
+
+listEl.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listEl.innerHTML);
+}
+
+function showTask() {
+  listEl.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
